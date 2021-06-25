137b29fdfdeebee65ad641a406a744f841cd3790
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-06-25 18:42:23 +0200

    Add models

---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
