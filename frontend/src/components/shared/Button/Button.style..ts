b743bc7d78ad049b0f8316d90c55cb65500c81bb
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-06-27 20:16:28 +0200

    Style Button & ButtonPrimary

---
+++ frontend/src/components/shared/Button/Button.style..ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: inline-block;
+  background: ${({ theme }) => theme.colors.lightColor};
+  color: #333;
+  padding: 0.4rem 1.3rem;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+  margin-right: 0.5rem;
+  transition: opacity 0.2s ease-in;
+  outline: none;
+`;
+
+export const ButtonPrimary = styled(Button)`
+  background: ${({ theme }) => theme.colors.primaryColor};
+  color: #fff;
+`;
