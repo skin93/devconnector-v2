740d12a39ca94053826b6b8ac2a7716bfdff6705
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-06-28 17:58:22 +0200

    Change interfaces for types

--- frontend/src/types/FormDataType.ts
+++ frontend/src/types/FormDataType.ts
@@ -1,6 +1,6 @@
-export interface IFormData {
+export type FormDataType = {
   name?: string;
   email: string;
   password: string;
   password2?: string;
-}
+};
---
+++ frontend/src/types/UserType.ts
@@ -0,0 +1,9 @@
+export type UserType = {
+  _id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  createdAt: string;
+  updatedtAt: string;
+  token: string;
+};
