913d200656eacbbf44caf71636007b34823653af
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-06-28 17:59:52 +0200

    Add auth & alert reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import alertReducer from '../features/alert/alertSlice';
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
+    alert: alertReducer,
   },
 });
 
